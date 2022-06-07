c2bf56ebe28e5ebaec2d50db5b12a55385999083
Yang <sdada0112@gmail.com>
2022-06-07 20:32:49 +0900

    220607

--- server.py
+++ server.py
@@ -2,42 +2,64 @@ import socket
 import threading
 import sqlite3
 
-BUFSIZE = 1024
-host = '10.10.20.33'
+BUFSIZE = 1024 #버퍼사이즈
+host = '10.10.20.33' 
 port = 9015
 
 
-def getcon():
+def getcon(): #db와 연결 함수
     con = sqlite3.connect('edu.db')
     c=con.cursor()
     return con, c
    
-#0 id 1pw 2name
-def signup(sock):
+def signup(sock): #회원가입 처리 함수
     con, c = getcon()
     data = sock.recv(BUFSIZE).decode()
     print('data: '+data)
-    userdata = data.split('/')
+    userdata = data.split('/') #/기준으로 문자열 나누기
     print('data[3]: '+userdata[3])
-    if userdata[3] == 'tea':
-        c.execute('insert into teacherInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) 
+    if userdata[3] == 'tea': #선생님
+        c.execute('insert into teacherInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) #선생 테이블에 데이터 저장
         con.commit()
-    elif userdata[3] == 'stu':
-        c.execute('insert into teacherInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) 
+    elif userdata[3] == 'stu': #학생
+        c.execute('insert into studentInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) #학생 테이블에 데이터 저장
         con.commit()
 
-def login(sock):
+def login(sock): #로그인 처리 함수
     con, c = getcon()
-    data = sock.recv(BUFSIZE).decode()
-    print('data: '+data)
-
-def handleclnt(sock):
     while True:
         data = sock.recv(BUFSIZE).decode()
         print('data: '+data)
-        if data == '!signup':
+        userdata = data.split('/') # /기준으로 문자열 나누기
+        print('data[2]: '+userdata[2])
+        if userdata[2] == 'tea': #선생님
+            c.execute('select ID, PW from teacherInfo') #선생 테이블에서 정보 찾기
+            userInfo = c.fetchall()
+            for search in userInfo:
+                if userdata[0] == search[0] and userdata[1] == search[1]: #찾은 정보랑 입력이랑 일치시
+                    sock.send(('!ok/tea').encode()) #성공시 !ok보내기
+                    break
+                else:
+                    sock.send(('!no/tea').encode()) #실패시 !no 보내기
+                    continue
+        elif userdata[2] == 'stu': #학생
+            c.execute('select ID, PW from studentInfo') #학생 테이블에서 정보 찾기
+            userInfo = c.fetchall()
+            for search in userInfo:
+                if userdata[0] == search[0] and userdata[1] == search[1]: #찾은 정보랑 입력이랑 일치시
+                    sock.send(('!ok/stu').encode()) #성공시 !ok 보내기
+                    break
+                else:
+                    sock.send(('!no/stu').encode()) #실패시 !no 보내기
+                    continue    
+        break       
+def handleclnt(sock): # 클라정보 수신 스레드
+    while True:
+        data = sock.recv(BUFSIZE).decode()
+        print('data: '+data)
+        if data == '!signup': #!signup 받으면 회원가입 함수 실행
             signup(sock)
-        elif data == '!login':
+        elif data == '!login': #!login 받으면 로그인 함수 실행
             login(sock)
         elif not data:
             break
