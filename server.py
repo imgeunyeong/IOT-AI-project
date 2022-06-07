1d12d6507ff7a2b35c130561d60612093bd4cee8
Yang <sdada0112@gmail.com>
2022-06-07 19:26:08 +0900

    220607

--- server.py
+++ server.py
@@ -1,16 +1,45 @@
 import socket
 import threading
+import sqlite3
 
 BUFSIZE = 1024
-host = '127.0.0.1'
-port = 9061
+host = '10.10.20.33'
+port = 9015
+
+
+def getcon():
+    con = sqlite3.connect('edu.db')
+    c=con.cursor()
+    return con, c
+   
+#0 id 1pw 2name
+def signup(sock):
+    con, c = getcon()
+    data = sock.recv(BUFSIZE).decode()
+    print('data: '+data)
+    userdata = data.split('/')
+    print('data[3]: '+userdata[3])
+    if userdata[3] == 'tea':
+        c.execute('insert into teacherInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) 
+        con.commit()
+    elif userdata[3] == 'stu':
+        c.execute('insert into teacherInfo values (?, ?, ?, ?)', (userdata[0], userdata[1], userdata[2], userdata[3])) 
+        con.commit()
+
+def login(sock):
+    con, c = getcon()
+    data = sock.recv(BUFSIZE).decode()
+    print('data: '+data)
 
 def handleclnt(sock):
     while True:
-        data = sock.recv(BUFSIZE)
-        data = data.decode()
+        data = sock.recv(BUFSIZE).decode()
         print('data: '+data)
-        if not data:
+        if data == '!signup':
+            signup(sock)
+        elif data == '!login':
+            login(sock)
+        elif not data:
             break
 
 if __name__=='__main__':
