3e900af795bb213c17dadbcf565a0eceda1087fb
qqwin2005@naver.com <qqwin2005@naver.com>
2022-06-10 16:32:52 +0900

    소스코드 설명 소식에 놀라서 만든 주석 대량 추가 버전

--- student_client.py
+++ student_client.py
@@ -84,7 +84,7 @@ class regit(QDialog): #가입창
         else:
             QMessageBox.warning(self, 'Warning', '중복된 아이디입니다')
 
-    def check_pw(self):
+    def check_pw(self): # 비번 확인
         id=self.idbar.text()
         pw1=self.pwbar1.text()
         pw2=self.pwbar2.text()
@@ -95,7 +95,7 @@ class regit(QDialog): #가입창
             infoall= id + '/' + pw1 + '/' + name + '/'
             
             
-            if self.student.isChecked() : infoall=infoall+'stu'
+            if self.student.isChecked() : infoall=infoall+'stu' # 학생인지 선생님인지 판별
             elif self.teacher.isChecked() :
                 infoall=infoall+'tea'
             sock.sendall(infoall.encode())
@@ -111,70 +111,72 @@ class regit(QDialog): #가입창
         sock.send('!login'.encode())
 
 
-class teacherui(QDialog):
+class teacherui(QDialog): # 선생님 ui의 흔적
     def __init__(self):
         super().__init__()
         self.ui=uic.loadUi("teacher.ui",self)   
 
 
-class studentui(QMainWindow):
+class studentui(QMainWindow): # 학생 ui
     def __init__(self):
         super().__init__()
         self.stu_ui=uic.loadUi("student.ui",self)
         self.setWindowTitle("학생용 클라이언트")
-        self.button_click()
-        self.icon = QIcon('talk.png')
+        self.button_click() # 여러가지 작용 함수 더미
+        self.icon = QIcon('talk.png') # 프로그램 아이콘 추가 (토크* 에서 가져온 아이콘) (png 파일 없으면 안뜸)
         self.setWindowIcon(self.icon)
-        self.study_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-
+        self.study_widget.setEditTriggers(QAbstractItemView.NoEditTriggers) # 학습 테이블 위젯 수정 불가하게 설정
+        self.qna_widget.setEditTriggers(QAbstractItemView.NoEditTriggers) # qna 테이블 위젯 수정 불가하게 설정
 
     def widget_append(self): # 학습자료 추가
         self.stackedWidget.setCurrentIndex(1)
         i = 0
         con = sqlite3.connect("stu_client.db")
-        with con:
+        with con: # 데이터베이스 열기
             cur = con.cursor()
             rows = cur.execute('select * from dinosaur')
             for row in rows:
-                self.study_widget.setRowCount((i + 1))
-                changetype = list(row)
-                for j in range(7):
-                    self.study_widget.setItem(i, j, QTableWidgetItem(str(changetype[j])))
-                i += 1
+                self.study_widget.setRowCount((i + 1)) # 0번째에 명, 크기, 체중 등이 들어가 있으니 건너뛰고 1번째부터 데이터 추가
+                changetype = list(row) # 값을 집어넣기 위해 리스트화
+                for j in range(7): # 넣을 값이 7가지 range(7)로 for문 사용
+                    self.study_widget.setItem(i, j, QTableWidgetItem(str(changetype[j]))) # i 는 row값 j는 column값
+                i += 1 # 1번째 row가 채워지면 다음 row를 채우기 위해 1 씩 증가
 
     def enter_chatroom(self): # 채팅방으로 이동 및 상담 받고싶은 선생님 입력
         self.stackedWidget.setCurrentIndex(4)
-        super().__init__()
+        self.counseling_browser.clear()
+        sock.send('!chat'.encode())  # 채팅창 접속 신호 보냄
+        super().__init__() # QMainwindow를 상속 받아서 새로 생성
         self.setWindowTitle("상담하고 싶은 선생님 입력 후 ENTER")
-        self.setGeometry(400, 400, 400, 100)
+        self.setGeometry(400, 400, 400, 100) # mainwindow 위치 설정
         self.setStyleSheet("color: rgb(131, 56, 236); background-color: qlineargradient(spread:pad, x1:0.125, y1:0.892227, x2:0.865, y2:0.130727, stop:0.208333 rgba(128, 106, 174, 255), stop:0.645833 rgba(204, 106, 201, 255));border: 1.5px solid rgb(58, 134, 255);border-radius: 5px;")
-        self.choice_teacher = QLineEdit(self)
-        self.choice_teacher.setGeometry(60, 25, 280, 50)
-        self.choice_teacher.setFont(QFont('Ubuntu', 14))
-        self.choice_teacher.setStyleSheet("color:black;background-color:lavender;")
-        self.choice_teacher.returnPressed.connect(self.send_choice_msg)
-        self.show()
+        # pyqt에서 스타일 시트 설정 들어가서 내 맘대로 편집한다음에 코드 가져오기
+        self.choice_teacher = QLineEdit(self) # 상담할 선생님을 적기 위한 라인 에딧 추가
+        self.choice_teacher.setGeometry(60, 25, 280, 50) # 위치지정
+        self.choice_teacher.setFont(QFont('Ubuntu', 14)) # 폰트 지정 및 크기 지정
+        self.choice_teacher.setStyleSheet("color:black;background-color:lavender;") # 스타일시트
+        self.choice_teacher.returnPressed.connect(self.send_choice_msg) # 선생님 이름 적은 후 enter 누를 시 작용
+        self.show() # mainwindow 띄우기
 
     def send_choice_msg(self): # 서버로 실시간 상담 접속 정보를 보냄
-        self.close()
-        sock.send('!chat'.encode())
-        sock.send(f'{self.choice_teacher.text()}'.encode())
+        self.close() # 선생님 이름을 적었으니 창을 닫음
+        sock.send(f'{self.choice_teacher.text()}'.encode()) # 적었던 상담하고 싶은 선생님 이름 서버로 전송
         self.counseling_browser.append("선생님을 기다리는중...")
-        tea_msg = Thread(target=self.recv_msg)
+        tea_msg = Thread(target=self.recv_msg) # 서버로부터 채팅방 메시지 받는 스레드 구동
         tea_msg.start()
 
     def recv_msg(self): # 서버에서 실시간 상담 메시지를 받음
         while True:
-            a = sock.recv(1024).decode()
-            self.counseling_browser.append(a)
+            a = sock.recv(1024).decode() # 상담 채팅방 메시지 받기
+            self.counseling_browser.append(a) # 받은 메시지 브라우저에 추가
 
     def send_msg(self): # 서버로 실시간 상담 메시지를 보냄
-        if self.counseling_line.text() == '':
+        if self.counseling_line.text() == '': # 빈칸이면 무시
             pass
         else:
-            self.counseling_browser.append(self.counseling_line.text())
-            sock.send(self.counseling_line.text().encode())
-            self.counseling_line.clear()
+            self.counseling_browser.append(self.counseling_line.text()) # 라인에 적은 메시지 내 브라우저에 추가
+            sock.send(self.counseling_line.text().encode()) # 라인에 적은 메시지 서버로 전송
+            self.counseling_line.clear() # 보낸후 clear
 
     def quiz_page(self): # 문제만 할당하고 Db에 있는 문제와 답이 일치 할 때 정답처리 리스트에 넣어서 해야하나?
         self.stackedWidget.setCurrentIndex(2)
@@ -185,7 +187,7 @@ class studentui(QMainWindow):
         sock.send('!Q&A'.encode())
 
     def upload_question(self): # 질문 학생이 입력시 학생 DB에 저장하면서 서버로 질문을 보내고 테이블 위젯 최신화 (서버에서 답 받아오면 DB에 추가하고 다시 최신화 해야함)
-        i = 0
+        i = 0  # 학습하기와 동일 range값만 바꿔줌
         con = sqlite3.connect('stu_client.db')
         with con:
             cur = con.cursor()
@@ -200,8 +202,8 @@ class studentui(QMainWindow):
         sock.send(self.qna_line.text().encode())
         self.qna_line.clear()
 
-    def reload(self): # qna 창 새로고침 함수
-        i = 0
+    def reload(self): # qna 창 새로고침 함수 (서버에서 받아온 값 추가 되었을 때 새로고침)
+        i = 0 # 값 초기화 안해주면 여름철 모기처럼 무한 증식
         con = sqlite3.connect('stu_client.db')
         with con:
             cur = con.cursor()
@@ -219,14 +221,14 @@ class studentui(QMainWindow):
         self.question_button.clicked.connect(self.qna_page)
         self.chatroom_button.clicked.connect(self.enter_chatroom)
         self.qna_line.returnPressed.connect(self.upload_question)
-        self.back_button.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
+        self.back_button.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0)) # 흔들리지 않는 편안함 lambda
         self.back_button_2.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
         self.back_button_3.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
         self.back_button_4.clicked.connect(lambda:self.stackedWidget.setCurrentIndex(0))
         self.reload_btn.clicked.connect(self.reload)
         self.counseling_line.returnPressed.connect(self.send_msg)
 
-        header = self.study_widget.horizontalHeader()
+        header = self.study_widget.horizontalHeader()  # 수평 헤더 반환 # 학습하기 자료 각 칼럼 가장 긴 자료 기준으로 늘림
         header.setSectionResizeMode(0, QHeaderView.Stretch)
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
@@ -234,8 +236,8 @@ class studentui(QMainWindow):
         header.setSectionResizeMode(4, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(5, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(6, QHeaderView.ResizeToContents)
-        self.quiz_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.qna_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.quiz_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # 퀴즈 위젯 퀴즈, 답변 칼럼 절반씩 나눈것
+        self.qna_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # qna 위젯 question, answer 칼럼 절반씩 나눈것
 
 
 if __name__ == '__main__':
