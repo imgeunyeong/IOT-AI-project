1cacf8b0704c1579da6d95074b4ec1c48fa22c27
qqwin2005@naver.com <qqwin2005@naver.com>
2022-06-11 19:39:38 +0900

    퀴즈 답 맞추면 점수 주는것까지 추가

--- student_client.py
+++ student_client.py
@@ -131,6 +131,15 @@ class studentui(QMainWindow): # 학생 ui
         self.answer_list = [] # 퀴즈 답 담기용 리스트
         self.final_answer_list = [] # 학생이 적은 답 담기 리스트
         self.score = 0
+        self.qThread = recv()  # 왜셀프
+        self.qThread.sig.connect(self.print_data)
+        self.qThread.demon = True
+        self.qThread.start()
+
+    @pyqtSlot(str)  # 전달 받은 데이터의 타입이 str이다
+    def print_data(self, data):  # 전달 받은 데이터를 data에 넣는다.
+        self.counseling_browser.append(data)  # recv한 데이터 상담 화면에 추가
+
 
     def widget_append(self): # 학습자료 추가
         self.stackedWidget.setCurrentIndex(1)
@@ -160,19 +169,16 @@ class studentui(QMainWindow): # 학생 ui
         self.choice_teacher.setFont(QFont('Ubuntu', 14)) # 폰트 지정 및 크기 지정
         self.choice_teacher.setStyleSheet("color:black;background-color:lavender;") # 스타일시트
         self.choice_teacher.returnPressed.connect(self.send_choice_msg) # 선생님 이름 적은 후 enter 누를 시 작용
+
         self.show() # mainwindow 띄우기
 
     def send_choice_msg(self): # 서버로 실시간 상담 접속 정보를 보냄
         self.close() # 선생님 이름을 적었으니 창을 닫음
         sock.send(f'{self.choice_teacher.text()}'.encode()) # 적었던 상담하고 싶은 선생님 이름 서버로 전송
         self.counseling_browser.append("선생님을 기다리는중...")
-        tea_msg = Thread(target=self.recv_msg) # 서버로부터 채팅방 메시지 받는 스레드 구동
-        tea_msg.start()
 
-    def recv_msg(self): # 서버에서 실시간 상담 메시지를 받음
-        while True:
-            a = sock.recv(1024).decode() # 상담 채팅방 메시지 받기
-            self.counseling_browser.append(a) # 받은 메시지 브라우저에 추가
+
+
 
     def send_msg(self): # 서버로 실시간 상담 메시지를 보냄
         if self.counseling_line.text() == '': # 빈칸이면 무시
@@ -231,7 +237,8 @@ class studentui(QMainWindow): # 학생 ui
         con = sqlite3.connect('stu_client.db')
         with con:
             cur = con.cursor()
-            cur.execute(f'insert into qna values ("{self.qna_line.text()}", " ")')
+            cur.execute(f'insert into qna values ("{self.qna_line.text()}", " ")') # 학생 데이터베이스에 질문 내용 저장
+            sock.send(f'!update{self.qna_line.text()}'.encode()) # 서버로 질문 내용 전송
             rows = cur.execute('select * from qna')
             for row in rows:
                 self.qna_widget.setRowCount((i + 1))
@@ -281,8 +288,48 @@ class studentui(QMainWindow): # 학생 ui
         header.setSectionResizeMode(4, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(5, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(6, QHeaderView.ResizeToContents)
-        self.quiz_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # 퀴즈 위젯 퀴즈, 답변 칼럼 절반씩 나눈것
-        self.qna_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # qna 위젯 question, answer 칼럼 절반씩 나눈것
+        self.quiz_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) # 퀴즈 위젯 퀴즈, 답변 로우 절반씩 나눈것
+        self.qna_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # qna 위젯 question, answer 로우 절반씩 나눈것
+
+    def request(self):  # 초대 수락거절 팝업창
+        request = ok_no()
+        request.exec_()
+
+class recv(QThread):
+    sig = pyqtSignal(str)  # 사용자 정의 시그널 sig를 만듬 전달할 데이터의 타입이 str이다.
+
+    def __init__(self):  # 큐티 브라우저에 append 해야하니까 인자 줘야할것같은데
+        super().__init__()  # 쓰레드 쓸려고 상속받음
+        print('q쓰레드 확인')
+
+    def run(self):
+        while True:
+            print('q쓰레드 확인222')
+            serv_msg = sock.recv(1024).decode()
+            print(f'서버의메세지: {serv_msg}')
+            if serv_msg == '!invite/serv':
+                # classinst=teacherui()
+                # classinst.request()
+                classinst = studentui()
+                studentui.request(classinst)
+            else:
+                self.sig.emit(serv_msg)
+
+
+class ok_no(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("request.ui", self)
+        self.reButton1.clicked.connect(self.send_ok)
+        self.reButton2.clicked.connect(self.send_no)
+
+    def send_ok(self):
+        self.close()
+        sock.send("!ok".encode())
+
+    def send_no(self):
+        self.close()
+        sock.send("!no".encode())
 
 
 if __name__ == '__main__':
